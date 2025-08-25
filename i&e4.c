bda825599c96d210e6eead6aae25380e550d4605
jayminprajapati-82 <jaymin.prajapati1603@gmail.com>
2025-08-25 20:11:16 +0200

    Create i&e4.c

---
+++ i&e4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check divisibility by 7
+    if (num % 7 == 0) {
+        printf("\n%d is divisible by 7.\n", num);
+    } else {
+        printf("\n%d is not divisible by 7.\n", num);
+    }
+
+    return 0;
+}
