551af0102ad1a789931a0f70caac9a77b633c3e4
jayminprajapati-82 <jaymin.prajapati1603@gmail.com>
2025-08-25 19:48:09 +0200

    Create pg1.c

---
+++ pg1.c
@@ -0,0 +1,18 @@
+//C program :- add two numbers//
+#include <stdio.h>
+int main() 
+{
+    int a, b, c;
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    
+    c = a + b;
+    
+    printf("Sum: %d", c);
+    
+    return 0;
+}
