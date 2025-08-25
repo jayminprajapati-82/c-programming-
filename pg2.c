d664b4e273a0954a28313e26bde10011a2f2cb62
jayminprajapati-82 <jaymin.prajapati1603@gmail.com>
2025-08-25 19:49:49 +0200

    Create pg2.c

---
+++ pg2.c
@@ -0,0 +1,19 @@
+//C program :- substract two numbers//
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    
+    c = a - b;
+    
+    printf("Substract: %d", c);
+    
+    return 0;
+}
