6448acd7fdbf932749abb05c285af11cf50404ab
jayminprajapati-82 <jaymin.prajapati1603@gmail.com>
2025-08-25 19:50:36 +0200

    Create pg3.c

---
+++ pg3.c
@@ -0,0 +1,19 @@
+//C program :- Multiply two numbers//
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+    
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    
+    c = a * b;
+    
+    printf("Multiplication: %d", c);
+    
+    return 0;
+}
